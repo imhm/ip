8a0808f3cab6513aa7c570e6c64d5467eacb73e7
hong ming <imhongminggg@gmail.com>
2020-08-26 23:52:15 +0800

    Add markAsDone function and Task Class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,23 +14,42 @@ public class Duke {
                 " What can I do for you?\n");
 
         String line;
-        String[] list = new String[100];
+        Task[] taskList = new Task[100];
         int i = 0;
+        int taskNumber;
 
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")){
-            if (!line.equals("list")) {
-                list[i] = Integer.toString(i + 1) + ". " + line;
-                System.out.println("added: " + line);
-                i++;
-            } else {
-                for (String listNum : Arrays.copyOf(list, i)){
-                    System.out.println(listNum);
+
+            switch (line){
+
+            case "list":
+                printTaskList(taskList);
+                break;
+
+            default:
+                if(line.contains("done")){
+                    //get the task number from user input
+                    taskNumber = Integer.parseInt(line.substring(line.length() - 1));
+                    taskList[taskNumber - 1].markAsDone(); // -1 to cater for index start from 0
+                } else {
+                    taskList[i] = new Task(line);
+                    i++;
                 }
             }
+
             line = in.nextLine();
         }
+
         System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    public static void printTaskList(Task[] taskList){
+        int i = 1;
+        for (Task taskListNum : Arrays.copyOf(taskList, Task.getTotalTask())){
+            System.out.printf("%d." + "[" + taskListNum.getStatusIcon() + "] " + taskListNum.getDescription() + "\n", i);
+            i++;
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    private static int totalTask = 0;
+    private static Task[] taskList = new Task[100];
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        totalTask++;
+        System.out.println("added: " + description);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public static int getTotalTask() {
+        return totalTask;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done: \n" + 
+                    "[" + this.getStatusIcon() + "] " + this.getDescription());
+    }
+}
