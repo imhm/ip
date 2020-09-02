c89b9e10a993add95231fc5151c54e6230cb3950
hong ming <imhongminggg@gmail.com>
2020-09-02 23:51:51 +0800

    Code refactoring

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,85 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        printWelcomeMessage();
+        Task[] taskList = new Task[100];
+        handleUserInput(taskList);
+        printExitMessage();
+    }
+
+    public static void handleUserInput(Task[] taskList) {
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine().trim();
+
+        while (!userInput.equals("bye")){
+            if (userInput.equals("list")) {
+                userViewTaskList(taskList);
+            } else {
+                if(userInput.contains("done")){
+                    userCompleteTask(taskList, userInput);
+                } else {
+                    userAddTask(taskList, userInput);
+                }
+            }
+
+            userInput = in.nextLine().trim();
+        }
+    }
+
+    public static void userAddTask(Task[] taskList, String userInput) {
+        String[] command;
+
+        command =  userInput.split(" ",2);
+
+        int taskListNumber = Task.getTotalTask();
+
+        switch (command[0]){
+        case "todo":
+            taskList[taskListNumber] = new Todo(command[1]);
+            break;
+        case "deadline":
+            command = command[1].split("/by");
+            taskList[taskListNumber] = new Deadline(command[0], command[1]);
+            break;
+        case "event":
+            command = command[1].split("/at");
+            taskList[taskListNumber] = new Event(command[0], command[1]);
+            break;
+        default:
+            break;
+        }
+
+        printDukeBorder(true);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(taskList[taskListNumber]);
+        System.out.println("Your total tasks: " + Task.getTotalTask());
+        printDukeBorder(false);
+    }
+
+    public static void userCompleteTask(Task[] taskList, String userInput) {
+        int taskNumberCompleted;
+        taskNumberCompleted = Integer.parseInt(userInput.substring(userInput.length() - 1));
+        taskList[taskNumberCompleted - 1].markAsDone(); // -1 to cater for index starting from 0
+
+        printDukeBorder(true);
+        System.out.println("Good work! I've marked this task as done: \n" +
+                "[" + taskList[taskNumberCompleted - 1].getStatusIcon() + "] " + taskList[taskNumberCompleted - 1].getDescription());
+        printDukeBorder(false);
+    }
+
+    public static void userViewTaskList(Task[] taskList){
+        printDukeBorder(true);
+        System.out.println("This is your list of task(s):");
+        for (int i = 0; i < Task.getTotalTask(); i++){
+            System.out.printf("%d." + taskList[i] + "\n", i + 1);
+        }
+        printDukeBorder(false);
+    }
+
+    public static void printWelcomeMessage() {
+        printDukeBorder(true);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,72 +88,21 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
-                " What can I do for you?\n");
+                " What can I do for you?");
+        printDukeBorder(false);
+    }
 
-        String line;
-        String[] command;
-        Task[] taskList = new Task[100];
-        int taskListNumber = 0;
-        int taskNumberCompleted;
-
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-
-        while (!line.equals("bye")){
-
-            switch (line.trim()){
-            case "list":
-                printTaskList(taskList);
-                break;
-
-            default:
-                if(line.contains("done")){
-                    //get the task number from user input
-                    taskNumberCompleted = Integer.parseInt(line.substring(line.length() - 1));
-                    taskList[taskNumberCompleted - 1].markAsDone(); // -1 to cater for index starting from 0
-
-                    printDukeBorder();
-                    System.out.println("Good work! I've marked this task as done: \n" +
-                            "[" + taskList[taskNumberCompleted - 1].getStatusIcon() + "] " + taskList[taskNumberCompleted - 1].getDescription());
-                } else {
-                    command =  line.split(" ",2);
-                    switch (command[0]){
-                    case "todo":
-                        taskList[taskListNumber] = new Todo(command[1]);
-                        break;
-                    case "deadline":
-                        command = command[1].split("/by");
-                        taskList[taskListNumber] = new Deadline(command[0], command[1]);
-                        break;
-                    case "event":
-                        command = command[1].split("/at");
-                        taskList[taskListNumber] = new Event(command[0], command[1]);
-                        break;
-                    }
-                    printDukeBorder();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(taskList[taskListNumber]);
-                    System.out.println("Your total tasks: " + Task.getTotalTask());
-                    taskListNumber++;
-                }
-            }
-
-            line = in.nextLine();
-        }
-        
-        printDukeBorder();
+    public static void printExitMessage() {
+        printDukeBorder(true);
         System.out.println("Bye. Hope to see you again soon!\n");
+        printDukeBorder(false);
     }
 
-    public static void printTaskList(Task[] taskList){
-        printDukeBorder();
-        System.out.println("This is your list of task(s):");
-        for (int i = 0; i < Task.getTotalTask(); i++){
-            System.out.printf("%d." + taskList[i] + "\n", i + 1);
+    public static void printDukeBorder(boolean top){
+        if (top) {
+            System.out.println("............. DUKE CHATBOX ^^ ............");
+        } else {
+            System.out.println("..........................................");
         }
-    }
-
-    public static void printDukeBorder(){
-        System.out.println("............. DUKE CHATBOX ^^ ............");
     }
 }
